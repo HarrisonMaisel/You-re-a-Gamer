72f232b731640bf37254720979348ce332942a49
Harrison Maisel <hmaisel35@gmail.com>
2020-01-15 16:07:46 -0600

    fixed empty alert msg

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,11 +1,6 @@
 import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
-const initialState = [
-  {
-    id: 1,
-    msg: 'Please log in'
-  }
-];
+const initialState = [];
 
 export default function(state = initialState, action) {
   const { type, payload } = action;
